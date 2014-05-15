051b007118af36665cb11a6ff37fb39106abcfa2
Alexandre Camsonne <camsonne@halla120.jlab.org>
2014-05-15 18:46:44 -0400

    fixed angle bounds

--- src/DVCS/TDDVCSGen.C
+++ src/DVCS/TDDVCSGen.C
@@ -9,16 +9,17 @@ TDDVCSGen::TDDVCSGen()
   fMTarg = 0.938;//GeV
   fGkp=new TGenPart(&fkp);
   pGp= fGkp;
-  pGp->SetThMin(-1);
-  pGp->SetThMax(1);
-  pGp->SetPhiMin(28);
-  pGp->SetPhiMax(32);
-  pGp->SetEMin(1);
-  pGp->SetEMax(1.5);
+  pGp->SetThMin(90-60e-3/TMath::Pi()*180);
+  pGp->SetThMax(90+60e-3/TMath::Pi()*180);
+  pGp->SetPhiMin(90-30-30e-3/TMath::Pi()*180);
+  pGp->SetPhiMax(90-30.+30e-3/TMath::Pi()*180);
+  pGp->SetEMin(4.);
+  pGp->SetEMax(4.2);
+  pGp->SetMass(0.000511);
   fGq2=new TGenPart(&fq2);
   fGL1=new TGenPart(&fL1);
   
-  fk.SetPxPyPzE(0,0,-11,11);
+  fk.SetPxPyPzE(0,11,0,11);
   fpi.SetXYZM(0,0,0,0.938);
 }
 
@@ -31,13 +32,14 @@ TDDVCSGen::~TDDVCSGen()
 
  void  TDDVCSGen::GenerateDDVCS()
 {
-  fGkp->Generate(0,1.5);//scattered electron
+  fGkp->Generate();//scattered electron
+  fGkp->Generate(4,4.2);//scattered electron
   fq1 = fk - fkp;
-  Double_t DTheta = 0.001;
-  Double_t DPhi = 1;
-  Double_t Mq=fGq2->Uniform(1,fq1.M());
-  fGq2->SetThMin(fq1.Theta()/TMath::Pi()*180+DTheta);
-  fGq2->SetThMax(fq1.Theta()/TMath::Pi()*180-DTheta);
+  Double_t DTheta = 12.2;
+  Double_t DPhi = 5;
+  Double_t Mq=fGq2->Uniform(1,fq1.M()-1);
+  fGq2->SetThMin(fq1.Theta()/TMath::Pi()*180-DTheta);
+  fGq2->SetThMax(fq1.Theta()/TMath::Pi()*180+DTheta);
   fGq2->SetPhiMin(fq1.Phi()/TMath::Pi()*180-DPhi);
   fGq2->SetPhiMax(fq1.Phi()/TMath::Pi()*180+DPhi);
   fGq2->Generate(5,sqrt(fq1.E()*fq1.E()-Mq*Mq));//generate virtual photon
@@ -45,14 +47,13 @@ TDDVCSGen::~TDDVCSGen()
   fpf= (fpi+fq1-fq2); // proton takes rest of impulsion
   DTheta = 0.01;
   DPhi = 10;
-  fGL1->SetThMin(fq2.Theta()/TMath::Pi()*180+DTheta);
-  fGL1->SetThMax(fq2.Theta()/TMath::Pi()*180-DTheta);
+  fGL1->SetThMin(fq2.Theta()/TMath::Pi()*180-DTheta);
+  fGL1->SetThMax(fq2.Theta()/TMath::Pi()*180+DTheta);
   fGL1->SetPhiMin(fq2.Phi()/TMath::Pi()*180-DPhi);
   fGL1->SetPhiMax(fq2.Phi()/TMath::Pi()*180+DPhi);
   fGL1->Generate(1,sqrt(fq2.E()*fq2.E()-(2*0.110*2*0.110)));
   fL1.SetE(sqrt(fL1.E()*fL1.E()+0.110*0.110));
-  fL2 = fq2 - fL1;
-  fL2.SetE(sqrt(fL2.E()*fL2.E()+0.110*0.110));
+  fL2.SetVectMag(fq2.Vect()-fL1.Vect(),0.110);
   Compute();
 }
   void TDDVCSGen:: GenerateDDVCSFlat(){}
@@ -137,7 +138,7 @@ void TGenPart::Generate()
    Vect.SetMagThetaPhi(En,Th,Ph);
    if (fLorVect)
      {
-       fLorVect->SetVectMag(Vect,TMath::Sqrt(En*En+fMass*fMass));
+       fLorVect->SetVectMag(Vect,fMass);
      }
 }
 
@@ -152,7 +153,7 @@ void TGenPart::GenerateDir(TVector3 dir )
    Vect.SetMagThetaPhi(En,Th,Ph);
    if (fLorVect)
      {
-       fLorVect->SetVectMag(Vect,TMath::Sqrt(En*En+fMass*fMass));
+       fLorVect->SetVectMag(Vect,fMass);
      }
 }
 
@@ -161,13 +162,17 @@ void TGenPart::Generate(Double_t Emin,Double_t Emax)
 {
    
    Double_t Th,Ph,En;
-   //if (fThMin!=0 && fThMin!=0)      
-       Th = Uniform(fThMin/180*TMath::Pi(),fThMax/180*TMath::Pi());
+   if (fThMin>fThMax) {Th = fThMin ; fThMin=fThMax; fThMax=Th;}   
+    if (fPhMin>fPhMax) {Ph = fPhMin ; fPhMin=fPhMax; fPhMax=Th;}   
+       Th = Uniform(fThMin,fThMax);
        //if (fPhMin!=0 && fPhMin!=0)      
-     Ph = Uniform(fPhMin/180*TMath::Pi(),fPhMax/180*TMath::Pi());
+     Ph = Uniform(fPhMin,fPhMax);
    En = Uniform(Emin,Emax);
     TVector3 Vect;
-   Vect.SetMagThetaPhi(En,Th,Ph);
+   
+    //   cout<<Th<<" "<<Ph<<" "<< En <<endl;
+
+   Vect.SetMagThetaPhi(En,Th/180*TMath::Pi(),Ph/180*TMath::Pi());
    if (fLorVect)
      {
        fLorVect->SetVectMag(Vect,fMass);
