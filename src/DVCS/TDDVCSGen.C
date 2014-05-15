b5aaf9aba84379818cbe52f3b9d7c05a7ca2c5a7
Alexandre Camsonne <camsonne@halla120.jlab.org>
2014-05-15 00:36:53 -0400

    angle fixed. Mag is mass and not energy

--- src/DVCS/TDDVCSGen.C
+++ src/DVCS/TDDVCSGen.C
@@ -1,11 +1,23 @@
 #include "TDDVCSGen.h"
 
+#define deg 1./180.*TMath::Pi()
+#define rad 180./TMath::Pi()
+
 TDDVCSGen::TDDVCSGen()
 {
+  TGenPart * pGp;
   fMTarg = 0.938;//GeV
   fGkp=new TGenPart(&fkp);
+  pGp= fGkp;
+  pGp->SetThMin(-1);
+  pGp->SetThMax(1);
+  pGp->SetPhiMin(28);
+  pGp->SetPhiMax(32);
+  pGp->SetEMin(1);
+  pGp->SetEMax(1.5);
   fGq2=new TGenPart(&fq2);
   fGL1=new TGenPart(&fL1);
+  
   fk.SetPxPyPzE(0,0,-11,11);
   fpi.SetXYZM(0,0,0,0.938);
 }
@@ -19,14 +31,22 @@ TDDVCSGen::~TDDVCSGen()
 
  void  TDDVCSGen::GenerateDDVCS()
 {
-  fGkp->Generate(0,3);//scattered electron
+  //fGkp->Generate(0,3);//scattered electron
   fq1 = fk - fkp;
-  fGq2->Generate(0.5,fq1.E());//generate virtual photon
-  fpf= (fp+fq1-fq2); // proton takes rest of impulsion
-  fGL1->SetThMin(fq2.Theta()-10);
-  fGL1->SetThMax(fq2.Theta()-10);
-  fGL1->SetThMin(fq2.Theta()+10);
-  fGL1->SetThMax(fq2.Theta()+10);
+  Double_t DTheta = 0.001;
+  Double_t DPhi = 1;
+  fGq2->SetThMin(fq1.Theta()/TMath::Pi()*180+DTheta);
+  fGq2->SetThMax(fq1.Theta()/TMath::Pi()*180-DTheta);
+  fGq2->SetPhiMin(fq1.Phi()/TMath::Pi()*180-DPhi);
+  fGq2->SetPhiMax(fq1.Phi()/TMath::Pi()*180+DPhi);
+  fGq2->Generate(8,fq1.E());//generate virtual photon
+  fpf= (fpi+fq1-fq2); // proton takes rest of impulsion
+  DTheta = 0.01;
+  DPhi = 10;
+  fGL1->SetThMin(fq2.Theta()/TMath::Pi()*180+DTheta);
+  fGL1->SetThMax(fq2.Theta()/TMath::Pi()*180-DTheta);
+  fGL1->SetPhiMin(fq2.Phi()/TMath::Pi()*180-DPhi);
+  fGL1->SetPhiMax(fq2.Phi()/TMath::Pi()*180+DPhi);
   fGL1->Generate(1,fq2.E());
   fL2 = fq2 - fL1;
   Compute();
@@ -47,7 +67,7 @@ TDDVCSGen::~TDDVCSGen()
 
  TLorentzVector TDDVCSGen::SetBeam(Double_t E)
   {
-    fk.SetPxPyPzE(0,0,-E,E);
+    fk.SetPxPyPzE(0,E,0,E);
   }
 
   TLorentzVector TDDVCSGen::SetScattered(TLorentzVector & v)
@@ -71,7 +91,29 @@ TDDVCSGen::~TDDVCSGen()
 
 void TDDVCSGen::Print()
 {
-  cout<<"Q2:"<<fQ2<<" xbj:"<<"Q2:"<<fQ2<<" xbj:"<<endl;
+  cout<<"Q2:"<<fQ2<<" xbj:"<<fxbj<<endl;
+  cout<<"fk :";fk.Print();
+  cout<<"fkp :";fkp.Print();
+  cout<<"fq1 :";fq1.Print();
+  cout<<"fq2 :";fq2.Print();
+  cout<<"fpi :";fpi.Print();
+  cout<<"fpf :";fpf.Print();
+  cout<<"fp :";fp.Print();
+  cout<<"fq :";fq.Print();
+  cout<<"fL1 :";fL1.Print();
+  cout<<"fL2 :";fL2.Print();
+  cout<<"fDelta :"; fDelta.Print();
+  cout<<"fk :";fk.Vect().Print();
+  cout<<"fkp :";fkp.Vect().Print();
+  cout<<"fq1 :";fq1.Vect().Print();
+  cout<<"fq2 :";fq2.Vect().Print();
+  cout<<"fpi :";fpi.Vect().Print();
+  cout<<"fpf :";fpf.Vect().Print();
+  cout<<"fp :";fp.Vect().Print();
+  cout<<"fq :";fq.Vect().Print();
+  cout<<"fL1 :";fL1.Vect().Print();
+  cout<<"fL2 :";fL2.Vect().Print();
+  cout<<"fDelta :"; fDelta.Vect().Print();
 }
 
 ClassImp(TDDVCSGen)
@@ -95,18 +137,36 @@ void TGenPart::Generate()
      }
 }
 
+void TGenPart::GenerateDir(TVector3 dir )
+{
+   
+   Double_t Th,Ph,En;
+   Th = Uniform(-fThMin/180*TMath::Pi()+dir.Theta(),fThMax/180*TMath::Pi()+dir.Theta());
+   Ph = Uniform(-fPhMin/180*TMath::Pi()+dir.Phi(),fPhMax/180*TMath::Pi()+dir.Phi());
+   En = Uniform(fEMin,fEMax);
+   TVector3 Vect;
+   Vect.SetMagThetaPhi(En,Th,Ph);
+   if (fLorVect)
+     {
+       fLorVect->SetVectMag(Vect,TMath::Sqrt(En*En+fMass*fMass));
+     }
+}
+
+
 void TGenPart::Generate(Double_t Emin,Double_t Emax)
 {
    
    Double_t Th,Ph,En;
-   Th = Uniform(fThMin/180*TMath::Pi(),fThMax/180*TMath::Pi());
-   Ph = Uniform(fPhMin/180*TMath::Pi(),fPhMax/180*TMath::Pi());
+   if (fThMin!=0 && fThMin!=0)      
+       Th = Uniform(fThMin/180*TMath::Pi(),fThMax/180*TMath::Pi());
+   if (fPhMin!=0 && fPhMin!=0)      
+     Ph = Uniform(fPhMin/180*TMath::Pi(),fPhMax/180*TMath::Pi());
    En = Uniform(Emin,Emax);
     TVector3 Vect;
    Vect.SetMagThetaPhi(En,Th,Ph);
    if (fLorVect)
      {
-       fLorVect->SetVectMag(Vect,TMath::Sqrt(En*En+fMass*fMass));
+       fLorVect->SetVectMag(Vect,fMass);
      }
 }
 
--- src/DVCS/TestKin.C
+++ src/DVCS/TestKin.C
@@ -1,8 +1,32 @@
+//#include "TDDVCSGen.C"
+
+ TFile * out = new TFile("out.root","RECREATE");
+  TTree * tree = new TTree("T","Generated events",99);
+
 void TestKin()
 {
   gROOT->ProcessLine(".L TDDVCSGen.C+");
   TVector3 ekp;
-  TDDVCSGen a;
-  ekp.SetMagThetaPhi(3,30/180.*TMath::Pi(),0);
-  
+  // ekp.SetXYZ(1.32*sin(30/180.*TMath::Pi()),1.32*cos(30/180.*TMath::Pi()),0);
+  ekp.SetMagThetaPhi(1.32,TMath::Pi()/2,(90-30)/180.*TMath::Pi());
+  // ekp.RotateX(90/180.*TMath::Pi());
+  TDDVCSGen *a = new TDDVCSGen();
+  TLorentzVector Fkp;
+  Fkp.SetVectM(ekp,0);
+  // a.SetBeam(0,11,0,11);
+  a->SetBeam(11);
+  a->SetScattered(Fkp);
+  a->Compute();
+  cout<<a.Q2()<<" "<<a.xbj()<<endl;
+  cout<<4*1.32*11*sin(15/180.*TMath::Pi())*sin(15/180.*TMath::Pi())<<" "<<4*1.32*11*sin(15/180.*TMath::Pi())*sin(15/180.*TMath::Pi())/(2*0.938*(11-1.32))<<endl;
+  a->GenerateDDVCS();
+  a->Print();
+  tree->Branch("ddvcs",a,32000,99);
+  for ( int i = 0 ; i< 10000 ; i++)
+    {
+       a->GenerateDDVCS();
+       tree->Fill();
+    }
+  tree->Write("T");
+  out->Close();
 }
