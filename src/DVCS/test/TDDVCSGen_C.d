15652f62f479de54c303050b6e192c394b7d1d6c
camsonne <camsonne@jlab.org>
2014-05-27 13:00:30 -0400

    test

---
+++ src/DVCS/test/TDDVCSGen_C.d
@@ -0,0 +1,44 @@
+
+# DO NOT DELETE
+
+./TDDVCSGen_C.so: TDDVCSGen.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TLorentzVector.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TMath.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/Rtypes.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/RConfig.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/RVersion.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/DllImport.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/Rtypeinfo.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/snprintf.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/strlcpy.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TGenericClassInfo.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TSchemaHelper.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TMathBase.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TError.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TVector3.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TVector2.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TObject.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TStorage.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TVersionCheck.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/Riosfwd.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TBuffer.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TMatrix.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TMatrixF.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TMatrixT.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TMatrixTBase.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TNamed.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TString.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TMatrixFBasefwd.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TMatrixDBasefwd.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TVectorFfwd.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TVectorDfwd.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TMatrixTUtils.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TMatrixFfwd.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TMatrixFUtils.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TMatrixFUtilsfwd.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TRotation.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TLorentzRotation.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TRandom3.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/TRandom.h
+./TDDVCSGen_C.so: /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/cintdictversion.h /home/camsonne/jlab_software/devel/Linux_Fedora20-x86_64-gcc/root/5.34.13/root/include/root/RVersion.h
+TDDVCSGen_C__ROOTBUILDVERSION= 5.34/13
